1d1c2454b9798030e5fe80a53f6e99f90d040074
ar4er7 <vyacheslavdobr@gmail.com>
2024-05-30 19:33:02 +0300

    made a registration form with no logic

---
+++ store/products/models.py
@@ -0,0 +1,26 @@
+# import os
+# import django
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+# django.setup()
+
+from django.db import models
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Product: {self.name} | Categotry: {self.category.name}"
+
